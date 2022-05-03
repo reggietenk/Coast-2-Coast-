3e31261e4abcc2cc63ea2a51ac8ff600f7b15ea4
Daniel Mendez <soliddan211@gmail.com>
2022-05-02 21:30:11 -0400

    added some rework to login and signupform.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import Map from './components/Map';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import AppNavbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import AppNavbar from './components/Navbar';
 // import { Nav } from 'react-bootstrap';
 // import 'mapbox-gl/dist/mapbox-gl.css';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-// import { setContext } from "@apollo/client/link/context";
+import { setContext } from "@apollo/client/link/context";
 import './index.css' 
+// import { Switch ,Route, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // import Home from './pages/HomePage'
 // import SignupForm from './components/SignupForm'
@@ -18,44 +19,39 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
 });
 
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem('id_token');
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
-  request: operation => {
-    const token = localStorage.getItem('id_token');
-
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
 });
-  },
-  uri: '/graphql'
-});
-
-
 
 
 function App() {
   return (
     <ApolloProvider client={client}>
+      {/* <Map/> */}
         <Router>
-        {/* <AppNavbar /> */}
-        <div className="container">
-        <Route exact path='/' component={Map} />
-        </div>
-        </Router>
-        
+        <div className="map-container">
+        <Route>
+              <Route exact path="/" component={Map} />
+              {/* <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+               
+      
+
+              {/* <Route component={NoMatch} /> */}
+            </Route>
+      </div>
+      </Router>
     </ApolloProvider>
   );
 }
