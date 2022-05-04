d614a8cfb22d0a798bdd17047728c93b02673cab
Lewis <dsapione@gmail.com>
2022-05-03 23:19:44 -0400

    front-end talking to back-end

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,64 +1,73 @@
-// see SignupForm.js for comments
-import React, { useState, } from "react";
-import { Form, Button, Alert,  } from "react-bootstrap";
-// import { useMutation } from "@apollo/react-hooks";
-// import { LOGIN_USER } from "../utils/mutations";
-// import Auth from "../utils/auth";
-
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
-  // const [formState, setFormState] = useState({ email: '', password: '' });
-	// const [login, { error }] = useMutation(LOGIN_USER);
+  const [formState, setFormState] = useState({ email: '', password: '' });
+	const [login, { error }] = useMutation(LOGIN_USER);
 
 
-  // // update state based on form input changes
-  // const handleChange = (event) => {
-  //   const { name, value } = event.target;
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-  //   setFormState({
-  //     ...formState,
-  //     [name]: value,
-  //   });
-  // };
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-	// // submit form
-	// const handleFormSubmit = async event => {
-	// 	event.preventDefault();
+	// submit form
+	const handleFormSubmit = async event => {
+		event.preventDefault();
 
-	// 	try {
-	// 		const { data } = await login({
-	// 			variables: { ...formState }
-	// 		});
-
-	// 		Auth.login(data.login.token);
-	// 	} catch (e) {
-	// 		console.error(e);
-	// 	}
-	// };
+		try {
+			const { data } = await login({
+				variables: { ...formState }
+			});
+		
+			Auth.login(data.login.token);
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
   return (
-    <Form>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-      </Form.Text>
-    </Form.Group>
-  
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
+    <main className='flex-row justify-center mb-4'>
+      <div className='col-12 col-md-6'>
+        <div className='card'>
+          <h4 className='card-header'>Login</h4>
+          <div className='card-body'>
+            <form onSubmit={handleFormSubmit}>
+              <input
+                className='form-input'
+                placeholder='Your email'
+                name='email'
+                type='email'
+                id='email'
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                className='form-input'
+                placeholder='******'
+                name='password'
+                type='password'
+                id='password'
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button className='btn d-block w-100' type='submit'>
+                Submit
+              </button>
+            </form>
+						{error && <div>Login failed</div>}
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
-
 
 export default Login;
